f1a401b5310504429d41861588788b275d567ef8
rsolbes <70786055+rsolbes@users.noreply.github.com>
2025-11-03 10:25:31 -0600

    correcciones al backend + puertos del frontend.

--- backend/app.py
+++ backend/app.py
@@ -15,7 +15,7 @@ CORS(app, resources={r"/api/*": {"origins": "*"}})
 
 @app.after_request
 def after_request(response):
-    response.headers.add('Access-Control-Allow-Origin', 'http://localhost:4200')
+    response.headers.add('Access-Control-Allow-Origin', 'http://localhost:50687')
     response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
     response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
     response.headers.add('Access-Control-Allow-Credentials', 'true')
